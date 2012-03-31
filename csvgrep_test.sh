8427b113530191acedccc9e62a29b6426cbaf02c
gwenn <gtreguier@gmail.com>
2012-03-31 09:22:55 +0200

    Use 'go run'.

--- csvgrep_test.sh
+++ csvgrep_test.sh
@@ -2,7 +2,7 @@
 gzip -c test.csv > test.csv.gz
 bzip2 -c test.csv > test.csv.bz2
 
-$GOPATH/bin/csvgrep -s=, 'z' test.csv*
+go run csvgrep.go -s=, 'z' test.csv*
 
 rm test.csv.gz
 rm test.csv.bz2
@@ -11,11 +11,11 @@ echo
 echo 'Test TAB'
 tr ',' '\t' < test.csv > test.tsv
 
-$GOPATH/bin/csvgrep 'z' test.tsv
+go run csvgrep.go 'z' test.tsv
 
 rm test.tsv
 
 echo
 echo 'Test flags'
-$GOPATH/bin/csvgrep -w -i 'Z' test.csv
-$GOPATH/bin/csvgrep -i 'Z' test.csv
+go run csvgrep.go -w -i 'Z' test.csv
+go run csvgrep.go -i 'Z' test.csv
